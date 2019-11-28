7c247ee99193a1fd161329d0b74a8c29a5d77015
NicJr1110 <nicholasjnorton@gmail.com>
2019-11-28 14:09:06 +0000

    Set website to displays Stuff.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'; // This is what makes the file aware it is a react file. So always needed
-import Stuff from "./Stuff";  //This allows us access to header. bit like  namespace
+import Stuff from "./Stuff";
+  //This allows us access to header. bit like  namespace
 
 
 let App = () => (         //  the function has a capital as Babbel turns this into a Class later when turning the beneath into JS
