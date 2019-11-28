1a39fef83017b890398ca08b9befebd67a851fe6
NicJr1110 <nicholasjnorton@gmail.com>
2019-11-28 14:08:20 +0000

    Setup tricksy Challenge Thursday appify

---
+++ src/appify/AppifyMe.js
@@ -0,0 +1,11 @@
+import React from 'react'; // This is what makes the file aware it is a react file. So always needed
+import Pagination from "./Pagination" ;
+
+
+let AppifyMe = () => (         
+  <React.Fragment>        
+    <Pagination />   
+  </React.Fragment>
+);
+
+export default AppifyMe;
---
+++ src/appify/Pagination.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+let pages = [1,2,3,4];
+
+let Pagination = () => (
+    <ul className="pagination">
+    {pages.map((value, index) => (
+        <li key= {index}>
+            <a href={ "/page/" + value }>{value}</a>
+        </li>
+    ))}
+    </ul>
+);
+
+export default Pagination;
