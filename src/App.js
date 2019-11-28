6524b2722f9bafb4ef69b3c26c41ff39b7f5d032
NicJr1110 <nicholasjnorton@gmail.com>
2019-11-28 12:32:49 +0000

    create and add red square component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'; // This is what makes the file aware it is a react file. So always needed
 import Header from "./Header";  //This allows us access to header. bit like  namespace
-import Content from "./Content";
+import Square from "./Square";
 
 let App = () => (         //  the function has a capital as Babbel turns this into a Class later when turning the beneath into JS
   <React.Fragment>        
     <Header />      
-    <Content />
+    <Square />
   </React.Fragment>
 );
 
---
+++ src/Square.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const divStyle = {
+    background: 'red',
+    height: 200,
+    width: 200
+}
+
+let Square = () => (
+    <div style={divStyle}></div>
+);
+
+
+export default Square;
