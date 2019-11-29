e0cb2c07f1de41b50220206f95b9f532ba5fee82
NicJr1110 <nicholasjnorton@gmail.com>
2019-11-29 12:12:12 +0000

    Add colour change event onclck on square

--- src/Square.js
+++ src/Square.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Square = ({ colour }) => (
-    <div 
-        style={ { 
-        background: colour,
-        height: 200,
-        width: 200
-        } }
-        className='m-auto'
-    />
-);
+
+
+class Square extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            colour: 'papayawhip'
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+
+
+    }
+
+    handleClick() {
+        if(this.state.colour === 'papayawhip'){
+            this.setState({
+                colour : 'peru'
+            });
+        } else {
+            this.setState({
+                colour : 'papayawhip'
+            });
+        };
+    }
+ 
+    render(){
+        let { colour } = this.state;
+        return (
+            <div 
+                style={ { 
+                background: colour,
+                height: 200,
+                width: 200
+                } }
+                className='m-auto'
+                onClick={ this.handleClick }
+            />
+        );
+    }
+}
+
 
 
 //Defaulting props
